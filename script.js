93887302e99adefff6f78d518129f95380dc9eea
Eric Moras <121898077+Evickerest@users.noreply.github.com>
2023-10-17 15:49:52 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,22 +1,80 @@
 teamNumbers = document.querySelectorAll("li");
+teamSelected = document.querySelector("#team-selected");
+teamName = document.querySelector("#team-name");
+
+teamData = {
+    "wins": {},
+    "name": {}
+};
+
+document.addEventListener('DOMContentLoaded', () => {
+    
+    let teamName = {}
+    teamNumbers.forEach( team => {
+        let teamNumber = team.textContent;
+        fetch(`https://www.thebluealliance.com/api/v3/team/frc${teamNumber}`, {
+            method: "GET",
+            headers: {
+                "X-TBA-Auth-Key": "###",
+            
+            }
+        }).then( res => res.json()).then( json => {
+            teamName[teamNumber] = json["nickname"];
+        }).catch( err => console.log(err));
+    });
+    teamData["name"] = teamName;
+    
+    fetch(`https://www.thebluealliance.com/api/v3/event/2023miken/rankings`, {
+            method: "GET",
+            headers: {
+                "X-TBA-Auth-Key": "###",
+            
+            }
+        }).then( res => res.json()).then( json => {
+            calculateData(json)
+        }).catch( err => console.log(err));
+});
+
+function calculateData(json){
+    wins = {};
+    json["rankings"].forEach( team =>{
+       let teamNumber = team["team_key"].replace("frc","");
+       wins[teamNumber] = team["record"];
+    });
+    teamData["wins"] = wins;
+}
+
 
 //when selected, add the "active-button" class
 teamNumbers.forEach( team => {
     team.addEventListener( "click", ()=> {
-        //removes the active-button class from every other team button first
-        teamNumbers.forEach( team => team.classList.remove("active-button"));
-        team.classList.add("active-button"); 
+        selectTeam( team);
     });
 });
+
+
+function selectTeam( team ){
+    teamNumbers.forEach( team => team.classList.remove("active-button"));
+    team.classList.add("active-button"); 
+    teamSelected.textContent = team.textContent;
+    changeTeamName( team.textContent);
+}
+
+score = document.querySelector("#average-score");
+
+function changeTeamName(teamNumber){
+    teamName.textContent = teamData["name"][teamNumber];
+    winDict = teamData["wins"][teamNumber];
+    teamWinString = `Wins: ${winDict["wins"]} Losses: ${winDict["losses"]} Ties: ${winDict["ties"]}`
+    score.textContent = teamWinString;
+   
+}
 
 teamSearcher = document.querySelector("#team-searcher");
 noTeamFoundText = document.querySelector("#no-teams");
 
 //once a key is done, filter the search
 teamSearcher.onkeyup = filterTeamSearch;
-
-console.log(teamSearcher);
-
 
 function filterTeamSearch(){
     let numberFilter = teamSearcher.value;
@@ -26,7 +84,7 @@ function filterTeamSearch(){
     //isn't located in the team number, the display is set to none.
     for( i = 0; i < teamNumbers.length; i++){
         const team = teamNumbers[i];
-        
+
         if(team.textContent.indexOf(numberFilter) == 0){
            team.style.display = "";
            atleastOneTeam = true;
@@ -36,9 +94,21 @@ function filterTeamSearch(){
 
         //if no teams are found, display "No team Found"
         if(!atleastOneTeam) {
-        noTeamFoundText.style.display = "block";
+            noTeamFoundText.style.display = "block";
         } else {
-        noTeamFoundText.style.display = "none";
+            noTeamFoundText.style.display = "none";
         }
+    }
+}
+
+document.onkeydown = function(e){
+    if(e.code == "Enter"){
+        for( i = 0; i <  teamNumbers.length; i++){
+            if( teamNumbers[i].style.display != "none"){
+                selectTeam(teamNumbers[i]);
+                break;
+            }
+        }
+
     }
 }
